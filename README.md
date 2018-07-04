2797bf427b83cb22f576117ee63ee9b44c406893
Raymond Berglind <raymond.berglind@evry.com>
2018-07-04 11:27:13 +0200

    Updated README and began work on core.js.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # functional-tetris
+A Tetris implementation built with Javascript, using a functional approach. Originally based on [this tutorial](https://www.youtube.com/watch?v=H2aW5V46khA).
---
+++ src/core.js
@@ -0,0 +1,178 @@
+function createMatrix(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
+export function getInitialState() {
+    return {
+        arena: createMatrix(12, 20),
+
+        player: {
+            matrix: null,
+            position: {x: 0, y: 0},
+            score: 0
+        }
+    }
+}
+
+export function createPiece(type) {
+    if (type === 'T') {
+        return [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0]
+        ];
+    } else if (type === 'O') {
+        return [
+            [2, 2],
+            [2, 2]
+        ]
+    } else if (type === 'L') {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3]
+        ]
+    } else if (type === 'J') {
+        return [
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0]
+        ]
+    } else if (type === 'I') {
+        return [
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0]
+        ]
+    } else if (type === 'S') {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0]
+        ]
+    } else if (type === 'Z') {
+        return [
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0]
+        ]
+    }
+}
+
+export function arenaSweep(state) {
+    let rowCount = 1;
+    outer: for (let y = state.arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < state.arena[y].length; ++x) {
+             if (state.arena[y][x] === 0) {
+                 continue outer;
+             }
+        }
+
+        const row = state.arena.splice(y, 1)[0].fill(0);
+        state.arena.unshift(row);
+        ++y;
+
+        state.player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+
+    return state;
+}
+
+export function collide(arena, player) {
+    const [m, o] = [player.matrix, player.position];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                arena[y + o.y][x + o.x]) !== 0) {
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+export function merge(state) {
+    state.player.matrix.forEach((row, y) => {
+        row.forEach ((value, x) => {
+            if (value !== 0) {
+                state.arena[y + state.player.position.y][x + state.player.position.x] = value;
+            }
+        });
+    });
+    return state;
+}
+
+export function playerReset(state) {
+    const pieces = 'ILJOTSZ';
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.position.y = 0;
+    player.position.x = (arena[0].length / 2 | 0) -
+                        (player.matrix[0].length / 2 | 0);
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
+    }
+}
+
+export function playerMove(state, dir) {
+    state.player.position.x += dir;
+    if (collide(state.arena, state.player)) {
+        statee.player.position.x -= dir;
+    }
+    return state;
+}
+
+export function playerDrop() {
+    player.position.y++;
+    if (collide(arena, player)) {
+        player.position.y--;
+        merge(arena, player);
+        playerReset();
+        arenaSweep();
+        updateScore();
+    }
+    dropCounter = 0;
+}
+
+export function playerRotate(dir) {
+    const position = player.position.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.position.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.position.x = position;
+            return;
+        }
+    }
+}
+
+export function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x]
+            ] = [
+                matrix[y][x],
+                matrix[x][y]
+            ]
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
